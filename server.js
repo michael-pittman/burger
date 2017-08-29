c49b32fd7d9f09e08cee814652cd3506216e0e81
michaelc.pittman <michaelc.pittman@gmail.com>
2017-08-29 19:46:14 -0400

    switch to sequelize

--- server.js
+++ server.js
@@ -1,25 +1,22 @@
 var express = require("express");
-var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
-var exphbs = require("express-handlebars");
 
 var app = express();
-app.use(express.static(__dirname + "/public"));
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+var db = require("./models");
 
-app.use(methodOverride('_method'));
-app.engine('handlebars', exphbs({
-    defaultLayout: 'main'
-}));
-app.set('view engine', 'handlebars');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
-var routes = require('./controllers/routes.js');
-app.use('/', routes);
+app.use(express.static("public"));
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("connected")
+require("./routes/api-routes.js")(app);
+
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
