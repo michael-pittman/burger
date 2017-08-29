fda5f53d43dc861f47fed91d5da80d4c8de76893
michaelc.pittman <michaelc.pittman@gmail.com>
2017-08-29 19:49:23 -0400

    connections js edited

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,42 @@
-var mysql = require("mysql");
+//var mysql = require("mysql");
+//
+//var connection = mysql.createConnection({
+//  host: "localhost",
+//  user: "root",
+//  password: " ",
+//  database: "burger_db"
+//});
+//
+//connection.connect(function(err) {
+//  if (err) {
+//    console.error("error connecting: " + err.stack);
+//    return;
+//  }
+//  console.log("connected as id " + connection.threadId);
+//});
+//
+//module.exports = connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: " ",
-  database: "burger_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
+{
+  "development": {
+    "username": "root",
+    "password": " ",
+    "database": "burger_db",
+    "host": "localhost",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
   }
-  console.log("connected as id " + connection.threadId);
-});
-
-module.exports = connection;
+}
