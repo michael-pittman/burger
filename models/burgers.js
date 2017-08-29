0036f6a3ffad3492c1adbf38613b1d7a05a9329a
michaelc.pittman <michaelc.pittman@gmail.com>
2017-08-29 19:56:58 -0400

    modles

--- models/burgers.js
+++ models/burgers.js
@@ -1,19 +1,29 @@
-var orm = require('../config/orm.js');
+//var orm = require('../config/orm.js');
+//
+//var burger = {
+//    all: function(res){
+//        orm.all('burgers', function(results){
+//            res(results);
+//        })
+//    },
+//    
+//    update: function(id, res){
+//        orm.update('burgers',id,res);
+//    },
+//    
+//    create: function(name,res){
+//        orm.create('burgers', name, res);
+//    }
+//}
+//
+//module.exports = burger;
 
-var burger = {
-    all: function(res){
-        orm.all('burgers', function(results){
-            res(results);
-        })
-    },
-    
-    update: function(id, res){
-        orm.update('burgers',id,res);
-    },
-    
-    create: function(name,res){
-        orm.create('burgers', name, res);
-    }
-}
 
-module.exports = burger;
+
+module.exports = function(sequelize, DataTypes) {
+  var Burger = sequelize.define("burgers", {
+    burger_name: DataTypes.STRING,
+    devoured: DataTypes.BOOLEAN
+  });
+  return Burger;
+};
